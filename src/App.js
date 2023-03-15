34c4e0a43c0664b7d18615c3621279845b3c5717
pancham99 <pancham.047@gmail.com>
2023-03-15 10:48:12 +0530

    add resto page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 //componnent
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import Restaurant from './pages/Restaurant.js';
 //page
 import Home from "./pages/Home"
 
@@ -11,6 +12,11 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Home/>
+  },
+
+  {
+    path: "/restaurant",
+    element: <Restaurant/>
   },
  
 
