f8266a1e36f2824ae74a3c51804205b1146bcb3a
jagriti mishra <jagritimishra1110@gmail.com>
2023-03-15 19:20:10 +0530

    Restoo-work

--- src/pages/Restaurant.js
+++ src/pages/Restaurant.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Restomain from '../components/restojm/Restomain'
+import Restoslides from '../components/restojm/Restoslides'
 
 const Restaurant = () => {
   return (
-    <div>Restaurant</div>
+    <>
+   <Restoslides />
+   <Restomain />
+    </>
   )
 }
 
