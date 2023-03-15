de637ae04aec26c2b51b3e67564f4482ce791d3e
pancham99 <pancham.047@gmail.com>
2023-03-15 11:06:15 +0530

    add details

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link, link } from "react-router-dom"
 import { BsArrowsFullscreen, BsPeople } from "react-icons/bs"
 
 
 
 const Room = ({ room }) => {
-  const { id, image, imageLg, maxPerson, name, price, size } = room
+  const { id, image, imageLg, maxPerson, description, name, price, size } = room
   return (
     <div className='bg-white shadow-2xl mb-4 min-h-[500px] group'>
 
@@ -31,20 +32,34 @@ const Room = ({ room }) => {
           {/* room capacity */}
           <div className='flex justify-between w-[80%]'>
 
-              <div className='flex items-center gap-x-2'>
-                <div className='text-accent'>
-                  <BsPeople className='text-[15px]'/>
-                </div>
+            <div className='flex items-center gap-x-2'>
+              <div className='text-accent'>
+                <BsPeople className='text-[15px]' />
               </div>
+            </div>
 
-              <div className='flex gap-x-1'>
-                <div>max people</div>
-                <div>{maxPerson}</div>
-              </div>
+            <div className='flex gap-x-1'>
+              <div>max people</div>
+              <div>{maxPerson}</div>
+            </div>
 
           </div>
         </div>
       </div>
+
+      {/* name and descripation */}
+      <div className='text-center'>
+        <Link>
+          <h3 className='h3'>{name}</h3>
+        </Link>
+        <p className='max-w-[300px] mx-auto mb-3 lg:mb-6'>{description.slice(0,56)}</p>
+      </div>
+
+      {/* book button  */}
+    <Link className='btn btn-secondary btn-sm max-w-[240px] mx-auto
+    '>
+      Book now from ${price}
+    </Link>
     </div>
   );
 };
