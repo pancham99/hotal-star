680abd95262a788f5596d8b7f5779557701a85bd
jagriti mishra <jagritimishra1110@gmail.com>
2023-03-17 02:15:23 +0530

    card-work

--- src/components/restojm/Resto.js
+++ src/components/restojm/Resto.js
@@ -14,7 +14,7 @@ const Resto = ({ mish }) => {  //
         <img className='group-hover:scale-110 transition-all durration-300 w-full' src={imgsrc} alt="#" />
       </div>
       <div className='resto_info p-3'>
-        <span className='resto_name text-2xl'> {sname}</span>       
+        <span className='resto_name text-2xl capitalize'> {sname}</span>       
           <div className=' flex flex-row items-center gap-1' >
             <div className=' flex gap-1 py-2'>
             <a href='#' className=' bg-amber-800 h-4 w-4 rounded-full'></a>
