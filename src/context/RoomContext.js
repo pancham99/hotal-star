105f2bfa023b85e4751d99607cd705e2e753c8a2
pancham99 <pancham.047@gmail.com>
2023-03-17 14:46:41 +0530

    add kids and adults commponent

--- src/context/RoomContext.js
+++ src/context/RoomContext.js
@@ -10,11 +10,13 @@ export const RoomContext = createContext()
 
 const RoomProvider = ({ children }) => {
   const [rooms, setRooms] = useState(roomData);
+  const [adults, setAdults] = useState("1 Adults")
+  const [kids, setKids] = useState("0 kids")
 
   console.log(rooms)
 
 
-  return <RoomContext.Provider value={{rooms}}>
+  return <RoomContext.Provider value={{rooms, adults, setAdults, kids, setKids}}>
     {children}
   </RoomContext.Provider>
 };
