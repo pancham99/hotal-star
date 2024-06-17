f11a4bb45fe7b6d55e6583e1594733d76393dc7a
pancham99 <pancham.047@gmail.com>
2024-06-17 20:01:58 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,68 +1,73 @@
 import React from 'react';
 //componnent
-// import Header from "./components/Header"
-// import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 //page
 import Home from "./pages/Home"
-// import Restaurant from "./pages/Restaurant"
+import Restaurant from "./pages/Restaurant"
 
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-// import RoomDetails from './pages/RoomDetails';
-// import Spa from './pages/Spa';
-// import Contact from './pages/Contact';
-// import RestoDetails from './pages/RestoDetails';
-// import Tattva from './pages/Tattva';
-// import AboutAd from './pages/AboutAd';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import RoomDetails from './pages/RoomDetails';
+import Spa from './pages/Spa';
+import Contact from './pages/Contact';
+import RestoDetails from './pages/RestoDetails';
+import Tattva from './pages/Tattva';
+import AboutAd from './pages/AboutAd';
 
 
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Home/>
-//   },
-//   {
-//     path: "/restaurant",
-//     element: <Restaurant/>
-//   },
-//   {
-//     path: "/spa",
-//     element: <Spa/>
-//   },
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/restaurant",
+    element: <Restaurant/>
+  },
+  {
+    path: "/spa",
+    element: <Spa/>
+  },
 
-//   {
-//     path: "/about",
-//     element: <AboutAd/>
-//   },
+  {
+    path: "/about",
+    element: <AboutAd/>
+  },
   
-//   {
-//     path: "/tattva",
-//     element: <Tattva/>
-//   },
+  {
+    path: "/tattva",
+    element: <Tattva/>
+  },
 
 
-//   {
-//     path: "/contact",
-//     element: <Contact/>
-//   },
+  {
+    path: "/contact",
+    element: <Contact/>
+  },
+  {
+    path: "/rooms",
+    element: <Contact/>
+  },
 
-//   {
-//     path: "/room/:id",
-//     element: <RoomDetails/>
-//   },
-//   {
-//     path: "/resto/:id",
-//     element:<RestoDetails/>
-//   }
-// ])
+  {
+    path: "/room/:id",
+    element: <RoomDetails/>
+  },
+  {
+    path: "/resto/:id",
+    element:<RestoDetails/>
+  }
+])
 
 const App = () => {
   return <div>
-    {/* <Header/>
+    <Header/>
     <RouterProvider router={router}/>
-    <Footer/> */}
-    <Home/>
+    <Footer/>
+    {/* <Home/>  */}
+   
   </div>;
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,14 +7,14 @@ import Rooms from '../components/Rooms';
 const Home = () => {
   return (
     <div className=''>
-      <Header/>
+      <Header />
       <HeroSlider />
       <div className='container mx-auto relative'>
         <div className='bg-accent/20 mt-4 p-4 lg:shadow-xl lg:absolute left-0 right-0 lg:p-0 lg:z-30 lg:-top-12'>
-         <BookForm/>
+          <BookForm />
         </div>
       </div>
-      <Rooms />
+      {/* <Rooms /> */}
     </div>
   );
 };
